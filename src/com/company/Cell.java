3f419366e78426ac502cf6bd2bc2ddfca0f25789
QuentinLaure <quentin.laurent972@gmail.com>
2021-12-20 10:05:05 +0100

    Deleted Content.java class

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,26 +1,44 @@
 package com.company;
 
 public class Cell {
-    protected Content content;
+    protected String content;
+    protected String value;
 
     public Cell(String contentInput){
-        this.content = new Content(contentInput);
+        this.content = contentInput;
+        this.value = "DEFAULT VALUE FOR CONTENT";
     }
 
     public void setContent(String contentInput){
-        this.content.setInput(contentInput);
+        this.content = contentInput;
     }
 
     public String getContent(){
-        return this.content.getInput();
+        return this.content;
     }
 
     public String getValue(){
-        return this.content.getValue();
+        return this.value;
     }
 
     public void refreshValue(){
-        this.content.refreshValue();
+        //System.out.println("We refresh value from Content");
+        if (this.content == null) {
+            System.out.println("Cell Type : Null");
+            this.value = "Null";
+        } else if (this.content.charAt(0) == '=') {
+            System.out.println("Cell Type : String");
+            this.value = this.content;
+        } else {
+            try {
+                this.value = String.valueOf(Integer.parseInt(this.content));
+                System.out.println("Cell Type : Int");
+            } catch (NumberFormatException e) {
+                System.out.println("Cell Type : String");
+                this.value = this.content;
+
+            }
+        }
     }
 }
 
